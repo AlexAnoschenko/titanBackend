c2de6a8e7ff8ec635079906f0b7769fd7ceb316f
Aliaksei <=>
2022-05-13 18:00:51 +0300

    user service

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
+const router = require('./router/index');
 
 const PORT = process.env.PORT || 5555;
 const app = express();
@@ -10,6 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
+app.use('/api', router);
 
 const start = async () => {
   try {
